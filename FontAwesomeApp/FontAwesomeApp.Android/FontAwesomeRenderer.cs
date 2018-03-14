94893b2f4a70e00ef9621a99d283ce4a4b04c500
Paul Ballard <fifthstpartners@thed.com>
2018-03-14 03:20:43 -0500

    Initial Commit

---
+++ FontAwesomeApp/FontAwesomeApp.Android/FontAwesomeRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using FontAwesomeApp.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(FontAwesomeRenderer))]
+namespace FontAwesomeApp.Droid
+{
+	public class FontAwesomeRenderer : ButtonRenderer
+	{
+		private Context ctx;
+
+		public FontAwesomeRenderer(Context context) : base (context)
+		{
+			this.ctx = context;	
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+		{
+			base.OnElementChanged(e);
+			if (e.NewElement != null && e.NewElement.FontFamily == "Font Awesome 5 Free")
+			{
+				var font = Typeface.CreateFromAsset(this.ctx.ApplicationContext.Assets, "fa-regular-400.ttf");
+
+			}
+		}
+	}
+}
